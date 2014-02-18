8b7261ecf443605d2872620bf0f2b2fa335a371a
hoj201 <hoj201@gmail.com>
2014-02-18 22:47:13 +0000

    made the plot_some_fields_function plot streamlines

--- plot_some_fields.py
+++ plot_some_fields.py
@@ -2,18 +2,22 @@ import graphic_func as gf
 import matplotlib.pyplot as plt
 import numpy as np
 from jet_particle_functions import N,SIGMA,DIM
+import jet_particle_functions as jpf
 
-if N == 1:
-    q = np.zeros([N,DIM])
-    p = np.zeros([N,DIM])
-    p[0,0] = 1.
-    mu = np.zeros([N,DIM,DIM])
-    gf.display_velocity(q,p,mu)
-    plt.show()
-    plt.axis('equal')
-    gf.display_vorticity(q,p,mu)
-    plt.axis('equal')
-    plt.xlabel('x')
-    plt.show()
-else:
-    print 'N is not equal to 1, we opted to warn you of this rather than print any fields'
+state = np.load('./movies/dynamic_one_jet.npy')
+time = np.load('./movies/dynamic_one_jet_time.npy')
+
+step = 60
+q,p,mu = jpf.state_to_weinstein_darboux(state[step])
+print 'time = ' + str(time[step])
+gf.display_velocity(q,p,mu)
+plt.axis('equal')
+plt.show()
+
+gf.display_vorticity(q,p,mu)
+plt.axis('equal')
+plt.show()
+
+gf.display_streamlines(q,p,mu)
+plt.axis('equal')
+plt.show()
