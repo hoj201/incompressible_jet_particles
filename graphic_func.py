6ae5399e1a44441a7b487cfd6b68119d0d7e7a62
hoj201 <hoj201@gmail.com>
2014-02-18 22:46:49 +0000

    added a function to display streamlines

--- graphic_func.py
+++ graphic_func.py
@@ -8,7 +8,7 @@ SIGMA = jpf.SIGMA
 
 def display_velocity( q , p ,mu ):
  W = 5*SIGMA
- res = 50
+ res = 30
  N_nodes = res**DIM
  store = np.outer( np.linspace(-W,W , res), np.ones(res) )
  nodes = np.zeros( [N_nodes , DIM] )
@@ -27,6 +27,32 @@ def display_velocity( q , p ,mu ):
  plt.axis([- W, W,- W, W ])
  return plt.gcf()
 
+def display_streamlines( q , p ,mu ):
+ W = 5*SIGMA
+ res = 30
+ N_nodes = res**DIM
+ store = np.outer( np.linspace(-W,W , res), np.ones(res) )
+ nodes = np.zeros( [N_nodes , DIM] )
+ nodes[:,0] = np.reshape( store , N_nodes )
+ nodes[:,1] = np.reshape( store.T , N_nodes )
+ K,DK,D2K,D3K = jpf.derivatives_of_kernel(nodes , q)
+ vel_field = np.einsum('ijab,jb->ia',K,p) - np.einsum('ijabc,jbc->ia',DK,mu)
+ U = vel_field[:,0]
+ V = vel_field[:,1]
+ U2 = U.reshape(res,res)
+ V2 = V.reshape(res,res)
+ X = np.reshape( nodes[:,0] , [res,res])
+ Y = np.reshape( nodes[:,1] , [res,res])
+ speed = np.sqrt(U2*U2 + V2*V2)
+ lw = 5*speed / speed.max()
+ #plt.figure()
+ plt.streamplot( X , Y , U2 , V2  )
+ plt.plot(q[:,0],q[:,1],'ro')
+ for i in range(0,N):
+     plt.arrow(q[i,0], q[i,1], 0.2*p[i,0], 0.2*p[i,1], head_width=0.05, head_length=0.1, fc='b', ec='b')
+ plt.axis([- W, W,- W, W ])
+ return plt.gcf()
+ 
 def display_vorticity( q , p ,mu ):
  W = 5*SIGMA
  res = 100
