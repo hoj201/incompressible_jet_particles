39f693e965a6609ba0400bf5cc3e3a9b2bf7415f
hoj201 <hoj201@gmail.com>
2014-02-27 16:19:39 +0000

    i dunno

--- plot_some_fields.py
+++ plot_some_fields.py
@@ -4,21 +4,21 @@ import numpy as np
 from jet_particle_functions import N,SIGMA,DIM
 import jet_particle_functions as jpf
 
-state = np.load('./movies/dynamic_one_jet.npy')
-time = np.load('./movies/dynamic_one_jet_time.npy')
+state = np.load('./movies/coasting_zero_jet_data.npy')
+time = np.load('./movies/coasting_zero_jet_time.npy')
 
 step = 0
 q,p,mu = jpf.state_to_weinstein_darboux(state[step])
 print 'time = ' + str(time[step])
 
 gf.display_velocity(q,p,mu)
-plt.axis('equal')
+#plt.axis('equal')
 plt.show()
 
-gf.display_vorticity(q,p,mu)
-plt.axis('equal')
-plt.show()
+#gf.display_vorticity(q,p,mu,quiver="on")
+#plt.axis('equal')
+#plt.show()
 
-gf.display_streamlines(q,p,mu)
-plt.axis('equal')
-plt.show()
+#gf.display_streamlines(q,p,mu)
+#plt.axis('equal')
+#plt.show()
