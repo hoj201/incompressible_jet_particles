02f42e54d435f519cb81d035c2d73d70a42829a2
hoj201 <hoj201@gmail.com>
2014-02-27 16:18:57 +0000

    made arrows smaller

--- graphic_func.py
+++ graphic_func.py
@@ -19,10 +19,12 @@ def display_velocity( q , p ,mu ):
  U = vel_field[:,0]
  V = vel_field[:,1]
  plt.figure()
- plt.quiver( nodes[:,0] , nodes[:,1] , U , V )
+ plt.quiver( nodes[:,0] , nodes[:,1] , U , V , scale=20 )
  plt.plot(q[:,0],q[:,1],'ro')
  for i in range(0,N):
-     plt.arrow(q[i,0], q[i,1], 0.2*p[i,0], 0.2*p[i,1], head_width=0.05, head_length=0.1, fc='b', ec='b')
+     plt.arrow(q[i,0], q[i,1], p[i,0], p[i,1], head_width=0.1, head_length=0.2, lw = 4.0, fc='b', ec='b')
+     plt.arrow(q[i,0], q[i,1], p[i,0], p[i,1], head_width=0.1, head_length=0.2, lw = 2.0, fc='w', ec='w')
+ plt.axis('equal')
  plt.axis([- W, W,- W, W ])
  return plt.gcf()
 
@@ -51,7 +53,7 @@ def display_streamlines( q , p ,mu ):
  plt.axis([- W, W,- W, W ])
  return plt.gcf()
  
-def display_vorticity( q , p ,mu ):
+def display_vorticity( q , p ,mu , quiver = None ):
  W = 5*SIGMA
  res = 100
  N_nodes = res**DIM
@@ -68,9 +70,23 @@ def display_vorticity( q , p ,mu ):
  X = np.reshape( nodes[:,0] , [res,res] )
  Y = np.reshape( nodes[:,1] , [res,res] )
  plt.figure()
- plt.contourf( X,Y, Z , cmap=plt.cm.bone )
- plt.plot(q[:,0],q[:,1],'ro')
+ plt.contourf( X,Y, Z , 10, cmap=plt.cm.rainbow )
+ plt.contour( X,Y, Z , 10, color='k' )
+ plt.plot(q[:,0],q[:,1],'wo')
  for i in range(0,N):
      plt.arrow(q[i,0], q[i,1], 0.2*p[i,0], 0.2*p[i,1], head_width=0.05, head_length=0.1, fc='b', ec='b')
+ if quiver=="on":
+  res = 30
+  N_nodes = res**DIM
+  store = np.outer( np.linspace(-W,W , res), np.ones(res) )
+  nodes = np.zeros( [N_nodes , DIM] )
+  nodes[:,0] = np.reshape( store , N_nodes )
+  nodes[:,1] = np.reshape( store.T , N_nodes )
+  K,DK,D2K,D3K = jpf.derivatives_of_kernel(nodes , q)
+  vel_field = np.einsum('ijab,jb->ia',K,p) - np.einsum('ijabc,jbc->ia',DK,mu)
+  U = vel_field[:,0]
+  V = vel_field[:,1]
+  plt.quiver( nodes[:,0] , nodes[:,1] , U , V , scale = 20 )
  plt.axis([- W, W,- W, W ])
+ plt.axis('equal')
  return plt.gcf()
