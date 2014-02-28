054d4f7abfb59ec03af9219577e31a34f44422c9
hoj201 <hoj201@gmail.com>
2014-02-28 11:34:36 +0000

    i dunno

--- make_movie.sh
+++ make_movie.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 echo "If you go to NYU film school you get to make cool movies like this."
+cd ./movie_frames
+rm *.png
+cd ..
 python generate_images.py
 ffmpeg -i ./movie_frames/frame_%d.png -loop 5 -pix_fmt yuv420p particle_motion.mp4
 open particle_motion.mp4
