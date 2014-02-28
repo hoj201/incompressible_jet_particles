8aa917ad5a08e96a32a032dfd11b98a074531619
hoj201 <hoj201@gmail.com>
2014-02-28 11:34:55 +0000

    i dunno

--- generate_images.py
+++ generate_images.py
@@ -13,7 +13,7 @@ N_timestep = y_data.shape[0]
 print 'generating png files'
 for k in range(0,N_timestep):
 	q,p,mu = jpf.state_to_weinstein_darboux( y_data[k] )
-	f = gf.display_vorticity(q,p,mu,quiver="on")
+	f = gf.display_velocity(q,p,mu)
 	time_s = str(time_data[k])
 	plt.suptitle('t = '+ time_s[0:4] , fontsize=16 , x = 0.75 , y = 0.25 )
 	fname = './movie_frames/frame_'+str(k)+'.png'
