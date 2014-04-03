7ad4b3c6027583017dc4bbff35c8b11cfdf91622
hoj201 <hoj201@gmail.com>
2014-04-03 18:56:40 +0100

    don't remember

--- plot_some_fields.py
+++ plot_some_fields.py
@@ -4,20 +4,33 @@ import numpy as np
 from jet_particle_functions import N,SIGMA,DIM
 import jet_particle_functions as jpf
 
-state = np.load('./movies/coasting_zero_jet_data.npy')
-time = np.load('./movies/coasting_zero_jet_time.npy')
+#coasting_zero_jet_data
+#zero_jet_tackles_another
+#dynamic_one_jet
 
-step = 0
-q,p,mu = jpf.state_to_weinstein_darboux(state[step])
-print 'time = ' + str(time[step])
+#state = np.load('./movies/zero_jet_tackles_another.npy')
+#time = np.load('./movies/zero_jet_tackles_another_time.npy')
+
+#step = 100
+spin = np.array([ [0. , -1.] , [1. , 0.]])
+stretch = np.array([ [0. , 1.] , [1. , 0.]])
+shear = np.array([ [0. , 0.] , [1. , 0.]])
+
+q = np.zeros([N,DIM])
+p = np.zeros([N,DIM])
+mu = np.zeros([N,DIM,DIM])
+mu[0] = spin
+#p[0,0] = 1.
+#q,p,mu = jpf.state_to_weinstein_darboux(state[step])
+#print 'time = ' + str(time[step])
 
 gf.display_velocity(q,p,mu)
-#plt.axis('equal')
+plt.axis('equal')
 plt.show()
 
-#gf.display_vorticity(q,p,mu,quiver="on")
-#plt.axis('equal')
-#plt.show()
+gf.display_vorticity(q,p,mu,quiver="on")
+plt.axis('equal')
+plt.show()
 
 #gf.display_streamlines(q,p,mu)
 #plt.axis('equal')
