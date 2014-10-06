8041baaa041e6ea907a3fe18810d7c41886494a3
Jaap Eldering <j.eldering@imperial.ac.uk>
2014-10-06 15:29:40 +0100

    Simplify movie script and make it compatible with both OSX and Linux with avconv.

--- make_movie.sh
+++ make_movie.sh
@@ -1,8 +1,11 @@
-#!/bin/bash
-echo "If you go to NYU film school you get to make cool movies like this."
-cd ./movie_frames
-rm *.png
-cd ..
+#!/bin/sh
+
+rm -f ./movie_frames/*.png
 python generate_images.py
-ffmpeg -i ./movie_frames/frame_%d.png -loop 5 -pix_fmt yuv420p particle_motion.mp4
-open particle_motion.mp4
+
+if [ `uname` = 'Linux' ]; then
+	avconv -y -i ./movie_frames/frame_%d.png  -pix_fmt yuv420p particle_motion.mp4
+else
+	ffmpeg -i ./movie_frames/frame_%d.png -loop 5 -pix_fmt yuv420p particle_motion.mp4
+	open particle_motion.mp4
+fi
