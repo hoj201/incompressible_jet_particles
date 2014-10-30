db77ec383800b876848d2743ad5dd8b2cb903dc2
Jaap Eldering <j.eldering@imperial.ac.uk>
2014-10-30 17:16:48 +0000

    Add notes about generated movies, and changes to code.

--- test_collision.py
+++ test_collision.py
@@ -30,24 +30,34 @@ mu = np.zeros([N,DIM,DIM])
 
 r0 = 3.0
 p0 = -1.5
-#d = 1.13*SIGMA
-d = 1.4 *SIGMA
-J = d*p0;
-omega = 0.3;
+d = 1.2*SIGMA
+J = d*p0
+omega = 0.1
 q[0] = [-r0,-d/2]
 q[1] = [ r0, d/2]
+#q[2] = [ -2, -3.5 ]
 p[0][0] = -p0
 p[1][0] =  p0
-#mu[0] =  omega*spin;
-#mu[1] = -omega*spin;
+#p[2][0] =  0.4
+mu[0] =  omega*spin
+mu[1] = -omega*spin
+#mu[2] = 0.2*spin
 
 T = 60.0
 
 #print 'testing various functions'
 #print  jpf.test_functions(1)
 
+def momentum(q,p,mu):
+	res = [ 0,0 ]
+	for i in range(N): res += p[i]
+	return res
+
+
 E0 = jpf.Hamiltonian(q,p,mu)
+p0 = momentum(q,p,mu)
 print 'initial energy is ' + str(E0)
+print 'initial momentum: ' + str(p0[0]) + ',' + str(p0[1])
 
 state =  jpf.weinstein_darboux_to_state( q , p , mu )
 step_max = 200
@@ -58,4 +68,6 @@ np.save('time_data',t_span)
 
 q,p,mu = jpf.state_to_weinstein_darboux( y_span[step_max-1] )
 Ef = jpf.Hamiltonian(q,p,mu)
+pf = momentum(q,p,mu)
 print '  final energy is ' + str(Ef) + '  diff = ' + str(Ef-E0)
+print '  final momentum: ' + str(p0[0]) + ',' + str(p0[1])
