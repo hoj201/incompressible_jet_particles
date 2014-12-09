a51f59beef43eb6519ca3954493ddac9a2eef237
Jaap Eldering <j.eldering@imperial.ac.uk>
2014-12-09 13:40:39 +0000

    Generate frame filenames with leading zeros to preserve order and speed up things by processing them in parallel.

--- generate_images.py
+++ generate_images.py
@@ -2,21 +2,26 @@ import jet_particle_functions as jpf
 import graphic_func as gf
 import matplotlib.pyplot as plt
 import numpy as np
+import sys
+import multiprocessing as mp
 
 y_data = np.load('./state_data.npy')
 time_data = np.load('./time_data.npy')
 
-#y_data = np.load('state_data.npy')
-#time_data = np.load('time_data.npy')
-#print 'shape of y_data is ' + str( y_data.shape )
-N_timestep = y_data.shape[0]
-print 'generating png files'
-for k in range(0,N_timestep):
-	q,p,mu = jpf.state_to_weinstein_darboux( y_data[k] )
+def write_image(k):
+	q,p,mu,q1 = jpf.state_to_weinstein_darboux( y_data[k] )
 	f = gf.display_velocity(q,p,mu)
 	time_s = str(time_data[k])
 	plt.suptitle('t = '+ time_s[0:4] , fontsize=16 , x = 0.75 , y = 0.25 )
-	fname = './movie_frames/frame_'+str(k)+'.png'
+	fname = './movie_frames/frame_%03i.png' % k
 	f.savefig( fname )
 	plt.close(f)
-print 'done'
+	sys.stdout.write(' '+str(k))
+	sys.stdout.flush()
+
+N_timestep = y_data.shape[0]
+sys.stdout.write('generating png files:')
+sys.stdout.flush()
+p = mp.Pool(8)
+p.map(write_image,range(0,N_timestep))
+sys.stdout.write(' done\n')
--- make_movie.sh
+++ make_movie.sh
@@ -4,8 +4,8 @@ rm -f ./movie_frames/*.png
 python generate_images.py
 
 if [ `uname` = 'Linux' ]; then
-	avconv -y -i ./movie_frames/frame_%d.png  -pix_fmt yuv420p particle_motion.mp4
+	avconv -y -i ./movie_frames/frame_%03d.png  -pix_fmt yuv420p particle_motion.mp4
 else
-	ffmpeg -i ./movie_frames/frame_%d.png -loop 5 -pix_fmt yuv420p particle_motion.mp4
+	ffmpeg -i ./movie_frames/frame_%03d.png -loop 5 -pix_fmt yuv420p particle_motion.mp4
 	open particle_motion.mp4
 fi
